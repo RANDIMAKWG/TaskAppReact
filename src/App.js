d8390553b36f9d7a45d114b3a26174be2deb756d
Kawinda Randima <randima@thesanmark.com>
2024-04-01 18:34:37 +0530

    task card was crated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './compononets/Task';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Hello World..!  </h1> 
 
-    </div>
+  const task =[
+    {id:1,task:"Task 1"},
+    {id:2,task:"Task 2"},
+    {id:3,task:"Task 3"},
+  ];
+
+  return (
+  <div className='app'> 
+    <Task task={task[0].task}/>
+    <Task task={task[1].task}/>
+    <Task task={task[2].task}/>
+  </div>
   );
+
 }
 
 export default App;
